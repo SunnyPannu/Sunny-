de91eb0c6151074e9ad69777fc1c6670289f8453
sunny <165862679+SunnyPannu@users.noreply.github.com>
2024-04-19 19:44:19 +1200

    Create lambda.py

---
+++ lambda.py
@@ -0,0 +1,18 @@
+def make_group(person1, person2):
+    group = []
+    return group
+
+make_group = lambda person1, person2: [person1, person2]
+
+person1 = "Sumit"
+person2 = "Sunny"
+
+group = make_group(person1, person2)
+print("Group members:")
+
+for person in group:
+    print(person) 
+
+for person in group:
+    print(person)
+
